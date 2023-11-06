ca5a296b1fd2d5173269cfe0c1aeb477f5af52a7
DSmith575 <SMITDE5@student.op.ac.nz>
2023-11-06 17:05:42 +1300

    build(component): added rough draft for super admin seeding basic users

---
+++ prisma/basicUsersSeed.js
@@ -0,0 +1,19 @@
+const loginPath = 'http:localhost3000/api/v1/login';
+
+const loginDetails = {
+    username: "SuAdmin1",
+    password: "password123"
+}
+
+const main = async () => {
+
+    try {
+    const response = await fetch(loginPath, loginDetails);
+    const data = await response.json();
+    console.log(data);
+} catch (error) {
+    console.log(error);
+}
+}
+
+main();
