530d56bb0163c16467efce6fffe9475684fbce36
Deacon Smith (1000106517) <SMITDE5@student.op.ac.nz>
2023-11-08 16:26:42 +1300

    build(component): added some comments for things to test with quiz creation

--- controllers/auth/quizzes/quizzes.js
+++ controllers/auth/quizzes/quizzes.js
@@ -1,5 +1,24 @@
 import { PrismaClient } from '@prisma/client';
+import axios from 'axios';
 
+/**
+ *       statusCode: res.statusCode,
+ *       statusCode: res.statusCode,
+ *       statusCode: res.statusCode,
+ *       statusCode: res.statusCode,
+ *       statusCode: res.statusCode,
+ *       statusCode: res.statusCode,
+ *       statusCode: res.statusCode,
+ *       statusCode: res.statusCode,
+ *       statusCode: res.statusCode,
+ *       statusCode: res.statusCode,
+ *       statusCode: res.statusCode,
+ *       statusCode: res.statusCode,
+ *       statusCode: res.statusCode,
+ *
+ *
+ *      CHANGE EMAIL TO COMPARE USERNAME
+ */
 const prisma = new PrismaClient();
 
 const createQuiz = async (req, res) => {
@@ -14,15 +33,51 @@ const createQuiz = async (req, res) => {
       });
     }
 
-    return res.json({
-      msg: name,
-      difficulty,
-      categoryId,
-      startDate,
-      endDate,
-      totalQuestions,
-      questionType,
+    const getQuiz = await fetch(
+      `https://opentdb.com/api.php?amount=${totalQuestions}&category=${categoryId}&difficulty=${difficulty}&type=${questionType}`,
+    );
+    const questions = await getQuiz.json();
+
+    // DO NOT NEED TO GET INCORRECT ANSWERS ONLY COMPARE GIVEN ANSWER WITH CORRECT ANSWER
+    // return res.json({
+    //   questions: data.results,
+    // });
+
+    // console.log(questions.results);
+
+    await prisma.category.create({
+      data: {
+        id: categoryId,
+        name: questions.results[0].category
+      },
     });
+
+    await prisma.quiz.create({
+      data: {
+        categoryId,
+        name,
+        type: questionType,
+        difficulty,
+        startDate,
+        endDate,
+        questions: questions.results,
+      },
+    });
+
+    return res.status(201).json({
+      msg: 'Quiz successfully created',
+    });
+
+    // return res.json({
+    //   msg: name,
+    //   difficulty,
+    //   categoryId,
+    //   startDate,
+    //   endDate,
+    //   totalQuestions,
+    //   questionType,
+    //   questions: data.results
+    // });
   } catch (error) {
     return res.status(500).json({
       msg: error.message,
@@ -60,4 +115,30 @@ const deleteQuiz = async (req, res) => {
   }
 };
 
-export { createQuiz, deleteQuiz };
+// Check categories
+// if id and name don't exist, create them, else pass a list of categories that are already made to use.
+
+const getQuiz = async (req, res) => {
+  try {
+    const userID = await prisma.category.findUnique({
+      where: { id: Number(req.params.id) },
+    });
+
+    if(!userID) {
+      return res.status(404).json({
+        msg: "No quiz found with that id",
+        data: await prisma.category.findMany()
+      })
+    }
+
+    return res.json({
+      data: userID,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      msg: 'No Quiz found',
+    });
+  }
+};
+
+export { createQuiz, deleteQuiz, getQuiz };
--- routes/quizzes/quiz.js
+++ routes/quizzes/quiz.js
@@ -8,7 +8,7 @@
 
 import express from 'express';
 import validateQuiz from '../../middleware/quizValidation/createQuizValidation.js';
-import { createQuiz, deleteQuiz } from '../../controllers/auth/quizzes/quizzes.js';
+import { createQuiz, deleteQuiz, getQuiz } from '../../controllers/auth/quizzes/quizzes.js';
 import auth from '../../middleware/authRoute.js';
 
 const router = express.Router();
@@ -16,5 +16,6 @@ const router = express.Router();
 // router.route('/').get(createQuiz);
 router.route('/create').post(auth, validateQuiz, createQuiz);
 router.route('/delete/:id').delete(auth, deleteQuiz);
+router.route('/:id').get(getQuiz);
 
 export default router;
